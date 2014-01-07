61d072d40ba7daafa854f5f5ea0f9a20be9f8fe4
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 22:01:02 +0900

    Scroll
    complete

--- p249/ViewController.m
+++ p249/ViewController.m
@@ -7,16 +7,54 @@
 //
 
 #import "ViewController.h"
+#define TAG_CONTENT 99
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 
 @end
 
 @implementation ViewController
+-(IBAction)fitToWidth:(id)sender{
+    UIView *contentView = [self.view viewWithTag:TAG_CONTENT];
+    float scale = self.scrollView.frame.size.width / contentView.frame.size.width;
+    NSLog(@"scale: %f",scale);
+    [self.scrollView setZoomScale:scale animated:YES];
+}
+-(IBAction)fitToHeight:(id)sender{
+    UIView *contentView = [self.view viewWithTag:TAG_CONTENT];
+    float scale = self.scrollView.frame.size.width / contentView.frame.size.width;
+    NSLog(@"scale: %f",scale);
+    [self.scrollView setZoomScale:scale animated:YES];
+}
+-(IBAction)scaleDouble:(id)sender{
+    float x = self.scrollView.contentOffset.x;
+    float y = self.scrollView.contentOffset.y;
+    float w = self.scrollView.frame.size.width/2;
+    float h = self.scrollView.frame.size.height/2;
+    
+    CGRect doubleRect = CGRectMake(x, y, w, h);
+    [self.scrollView zoomToRect:doubleRect animated:YES];
+    
+}
 
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return [self.view viewWithTag:TAG_CONTENT];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:@"c10" ofType:@"jpg"];
+    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+    imageView.tag = TAG_CONTENT;
+    
+    [self.scrollView addSubview:imageView];
+    self.scrollView.delegate = self;
+    self.scrollView.contentSize = imageView.frame.size;
+    self.scrollView.minimumZoomScale =self.scrollView.frame.size.width / imageView.frame.size.width;
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
